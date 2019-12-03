0bdee2daa17f111ef943ffd8089096017305316d
lostw <zhengzhenyu@wondersgroup.com>
2019-12-03 11:28:05 +0800

    增加ZZError

---
+++ Source/Component/ZZError.swift
@@ -0,0 +1,35 @@
+//
+//  ZZError.swift
+//  Alamofire
+//
+//  Created by William on 2019/12/3.
+//
+
+import UIKit
+
+public struct ZZError: Error, LocalizedError {
+    public enum Domain {
+        case unknown, network, custom(String)
+
+        public var description: String {
+            switch self {
+            case .network: return "network"
+            case .custom(let value): return value
+            default: return "unknown"
+            }
+        }
+    }
+    public var code: Int
+    public var message: String = ""
+    public var domain: Domain = .unknown
+
+    public init(code: Int = -1, message: String, domain: Domain = .unknown) {
+        self.code = code
+        self.message = message
+        self.domain = domain
+    }
+
+    public var errorDescription: String? {
+        return message
+    }
+}
