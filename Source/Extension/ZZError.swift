50598cd53615aa008e06701090d0c53bb831b4b1
lostw <zhengzhenyu@wondersgroup.com>
2020-07-08 10:17:34 +0800

    remove EaseError

--- Source/Extension/ZZError.swift
+++ Source/Extension/ZZError.swift
@@ -7,37 +7,37 @@
 
 import Foundation
 
-//public typealias ZZApiError = ZZError
-//public struct ZZError: Error, LocalizedError {
-//    public static let silence = ZZError(code: "-", message: "")
-//    public var code: String
-//    public var message: String
-//
-//    public init(code: String = "-1", message: String) {
-//        self.code = code
-//        self.message = message
-//    }
-//
-//    public var errorDescription: String? {
-//        return message
-//    }
-//}
-//
-//public extension ZZError {
-//    /// 数据结构不符合预期
-//    static let neFailToParse = ZZError(apiErrorCode: 1000)
-//    /// 未找到错误消息描述
-//    static let neNoErrorMessage = ZZError(apiErrorCode: 1001)
-//
-//    public init(apiErrorCode: Int, message: String? = nil) {
-//        let networkCode = "NE\(apiErrorCode)"
-//        let networkMessage = message ?? "[\(networkCode)]服务错误，请稍后再试"
-//        self.init(code: networkCode, message: networkMessage)
-//    }
-//
-//    public init(networkError: Error, statusCode: Int?) {
-//        let networkCode = "HT\(statusCode ?? -1)"
-//        let networkMessage = "[\(networkCode)]\(networkError.localizedDescription)"
-//        self.init(code: networkCode, message: networkMessage)
-//    }
-//}
+public typealias ZZApiError = ZZError
+public struct ZZError: Error, LocalizedError {
+    public static let silence = ZZError(code: "-", message: "")
+    public var code: String
+    public var message: String
+
+    public init(code: String = "-1", message: String) {
+        self.code = code
+        self.message = message
+    }
+
+    public var errorDescription: String? {
+        return message
+    }
+}
+
+public extension ZZError {
+    /// 数据结构不符合预期
+    static let neFailToParse = ZZError(apiErrorCode: 1000)
+    /// 未找到错误消息描述
+    static let neNoErrorMessage = ZZError(apiErrorCode: 1001)
+
+    public init(apiErrorCode: Int, message: String? = nil) {
+        let networkCode = "NE\(apiErrorCode)"
+        let networkMessage = message ?? "[\(networkCode)]服务错误，请稍后再试"
+        self.init(code: networkCode, message: networkMessage)
+    }
+
+    public init(networkError: Error, statusCode: Int?) {
+        let networkCode = "HT\(statusCode ?? -1)"
+        let networkMessage = "[\(networkCode)]\(networkError.localizedDescription)"
+        self.init(code: networkCode, message: networkMessage)
+    }
+}
